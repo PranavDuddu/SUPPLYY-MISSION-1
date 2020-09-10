f2369ff4a187987ffdc63e17aed3efba60c808f5
PranavDuddu <69907209+PranavDuddu@users.noreply.github.com>
2020-09-10 20:11:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
 	rectMode(CENTER);
 	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite=createSprite(width/2, 200, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:false});
 	World.add(world, packageBody);
 	
 
@@ -48,16 +48,15 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  keyPressed();
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-    
+    packageSprite.x= packageBody.position.x 
+    packageSprite.y= packageBody.position.y 
   }
 }
 
